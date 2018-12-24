d732b1c5c0de1fed0e0e1463f5325c38121c39c3
Luana Pimentel <luana.pimentel85@gmail.com>
2018-12-24 19:10:57 -0200

    Added a service discovery using Eureka

--- client-service/src/main/java/com/ifood/demo/ClientApplication.java
+++ client-service/src/main/java/com/ifood/demo/ClientApplication.java
@@ -1,22 +1,23 @@
 package com.ifood.demo;
 
+import com.ifood.demo.client.ClientEventHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.ifood.demo.client.ClientEventHandler;
-
 @SpringBootApplication
 @EnableJpaRepositories("com.ifood.demo")
+@EnableEurekaClient
 public class ClientApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
-	}
-	
-    @Bean
-    ClientEventHandler clientEventHandler() {
-        return new ClientEventHandler();
-    }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ClientApplication.class, args);
+  }
+
+  @Bean
+  ClientEventHandler clientEventHandler() {
+    return new ClientEventHandler();
+  }
 }
--- order-service/src/main/java/com/ifood/demo/OrderApplication.java
+++ order-service/src/main/java/com/ifood/demo/OrderApplication.java
@@ -1,22 +1,21 @@
 package com.ifood.demo;
 
+import com.ifood.demo.order.OrderEventHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.ifood.demo.order.OrderEventHandler;
-
 @SpringBootApplication
 @EnableMongoRepositories("com.ifood.demo")
 public class OrderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
-	}
-	
-    @Bean
-    OrderEventHandler orderEventHandler() {
-        return new OrderEventHandler();
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(OrderApplication.class, args);
+  }
+
+  @Bean
+  OrderEventHandler orderEventHandler() {
+    return new OrderEventHandler();
+  }
 }
