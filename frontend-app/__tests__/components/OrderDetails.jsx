0769b2e7dda81296c2332a4b1a2eac06e648e056
Luana Pimentel <luana.pimentel85@gmail.com>
2019-02-02 10:43:54 -0200

    Don't display the results table if there are no orders to show

--- frontend-app/__tests__/components/OrderDetails.jsx
+++ frontend-app/__tests__/components/OrderDetails.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import OrderDetails from "../../src/components/OrderDetails.jsx";
+import { OrderDetails } from "../../src/components/OrderDetails.jsx";
 import OrderSearch from "../../src/components/OrderSearch.jsx"
 import OrderSearchResults from "../../src/components/OrderSearchResults.jsx"
 import OrderItemsModal from "../../src/components/OrderItemsModal.jsx"
@@ -9,11 +9,27 @@ import OrderItemsModal from "../../src/components/OrderItemsModal.jsx"
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Order Details component", () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<OrderDetails />);
+        wrapper.setProps({
+            orders: [{}]
+        });
+    });
 
     it("should load components", () => {
-        const wrapper = shallow(<OrderDetails />);
         expect(wrapper.find(OrderSearch)).toHaveLength(1);
         expect(wrapper.find(OrderSearchResults)).toHaveLength(1);
+        expect(wrapper.find(OrderItemsModal)).toHaveLength(1);
+    });
+
+    it("should NOT load OrderSearchResults if there are no orders", () => {
+        wrapper.setProps({
+            orders: []
+        });
+        expect(wrapper.find(OrderSearch)).toHaveLength(1);
+        expect(wrapper.find(OrderSearchResults)).toHaveLength(0);
         expect(wrapper.find(OrderItemsModal)).toHaveLength(1);
     });
 });
